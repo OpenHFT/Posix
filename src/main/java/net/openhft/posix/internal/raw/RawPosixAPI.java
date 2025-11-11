e4f5d0383166108466bb10958c9b6f8280e3a6bd
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-11 15:07:48 +0000

    Update copyright headers to reflect 2013-2025 and SPDX-License-Identifier: Apache-2.0

--- src/main/java/net/openhft/posix/internal/raw/RawPosixAPI.java
+++ src/main/java/net/openhft/posix/internal/raw/RawPosixAPI.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
+ */
 package net.openhft.posix.internal.raw;
 
 import net.openhft.posix.PosixAPI;
--- src/test/java/net/openhft/posix/internal/jnr/MSyncFileBenchmarkMain.java
+++ src/test/java/net/openhft/posix/internal/jnr/MSyncFileBenchmarkMain.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
+ */
 package net.openhft.posix.internal.jnr;
 
 import net.openhft.posix.MMapFlag;
