cafa948a354d6d2db1de8bb636434abc2787e836
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-11 11:32:49 +0000

    Refactor copyright comments for consistency

--- src/main/java/net/openhft/posix/internal/raw/package-info.java
+++ src/main/java/net/openhft/posix/internal/raw/package-info.java
@@ -1,7 +1,6 @@
-//
-// Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
-//
-
+/*
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
+ */
 /**
  * Supplies a reflection based Posix API using raw JVM access.
  * <p>
